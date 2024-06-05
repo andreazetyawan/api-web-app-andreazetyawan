ea568a02ef9e71d734433ff6c7ff49b7b03121de
andreazetyawan <andreazetyawan@andreazetyawan.id>
2024-06-05 00:47:37 -0400

    fix bug folder strukture

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const cors = require("cors");
 
 const app = express();
 
-// const CORS_ORIGIN = process.env.ORIGIN
-
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: process.env.ORIGIN
 };
 
 app.use(cors(corsOptions));
