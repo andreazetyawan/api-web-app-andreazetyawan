495093c9f7918321b97045290ce2e205ef9d498f
andreazetyawan <andreazetyawan@andreazetyawan.id>
2024-06-04 21:20:07 -0400

    test express api

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to api web app andreazetyawan" });
 });
 
 // set port, listen for requests
